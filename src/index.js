0aec69dbc9f286db5a6c25e50533dd18cfebd35d
valery <valery.yuntsevich@yandex.ru>
2020-04-27 16:21:45 +0300

    release v1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 
 
 exports.cToF = function (c) {
-
+  let fahrenheit = parseFloat((c * 9 / 5 + 32).toFixed(2));
+  return fahrenheit
 }
 
 exports.fToC = function (f) {
-
+  let celsium = parseFloat(((f - 32) * 5 / 9).toFixed(2));
+  return celsium
 }
